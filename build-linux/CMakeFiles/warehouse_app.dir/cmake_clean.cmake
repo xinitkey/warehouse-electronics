0c48b8ce58636c50e88e67876e597e6766ab5553
xinitkey <nightmorsky666@gmail.com>
2026-04-03 09:00:26 +0300

    few features: add more new functions
    - add function edit , delete
    - add import / export CSV files
    - add statistic horizontal bar
    - fix search
    - add price , description of product

---
+++ build-linux/CMakeFiles/warehouse_app.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warehouse_app.dir/link.d"
+  "CMakeFiles/warehouse_app.dir/src/main.cpp.o"
+  "CMakeFiles/warehouse_app.dir/src/main.cpp.o.d"
+  "CMakeFiles/warehouse_app.dir/src/ui_main_frame.cpp.o"
+  "CMakeFiles/warehouse_app.dir/src/ui_main_frame.cpp.o.d"
+  "CMakeFiles/warehouse_app.dir/src/warehouse/db.cpp.o"
+  "CMakeFiles/warehouse_app.dir/src/warehouse/db.cpp.o.d"
+  "CMakeFiles/warehouse_app.dir/src/warehouse/storage.cpp.o"
+  "CMakeFiles/warehouse_app.dir/src/warehouse/storage.cpp.o.d"
+  "CMakeFiles/warehouse_app.dir/src/warehouse/ui_add_item_dialog.cpp.o"
+  "CMakeFiles/warehouse_app.dir/src/warehouse/ui_add_item_dialog.cpp.o.d"
+  "warehouse_app"
+  "warehouse_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/warehouse_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-linux/CMakeFiles/warehouse_app.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for warehouse_app.
+# This may be replaced when dependencies are built.
